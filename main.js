031442f3c15eb37710862926449ea0d58a47118f
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-05-14 19:20:45 +0530

    Specscart

---
+++ main.js
@@ -0,0 +1,12 @@
+const menuIcon = document.querySelector(".menu-icon");
+const navbarLinks = document.querySelector(".navbar-links");
+
+menuIcon.addEventListener("click", () => {
+  navbarLinks.classList.toggle("responsive");
+});
+const menuIcon = document.querySelector(".menu-icon");
+const navbarLinks = document.querySelector(".navbar-links");
+
+menuIcon.addEventListener("click", () => {
+  navbarLinks.classList.toggle("responsive");
+});
