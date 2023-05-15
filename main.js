12e00c3029e245fec5c7f46ce3424fb60175ea56
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-05-15 17:30:17 +0530

    first page

--- main.js
+++ main.js
@@ -4,9 +4,3 @@ const navbarLinks = document.querySelector(".navbar-links");
 menuIcon.addEventListener("click", () => {
   navbarLinks.classList.toggle("responsive");
 });
-const menuIcon = document.querySelector(".menu-icon");
-const navbarLinks = document.querySelector(".navbar-links");
-
-menuIcon.addEventListener("click", () => {
-  navbarLinks.classList.toggle("responsive");
-});
